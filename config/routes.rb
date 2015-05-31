ffeab1c8498b0b1bb2caf2b56773f2cfe711858c
Aldin Milanovic <aldin@atlantbh.com>
2015-05-31 17:03:25 +0200

    Added layouts

--- config/routes.rb
+++ config/routes.rb
@@ -31,6 +31,13 @@ REAgency::Application.routes.draw do
 
   resources :admins
 
+  match '/',    to: 'static_pages#home'
+  match '/help',    to: 'static_pages#help'
+  match '/about',   to: 'static_pages#about'
+  match '/contact', to: 'static_pages#contact'
+
+  match '/signup',  to: 'users#new'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -80,7 +87,7 @@ REAgency::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'admin#index'
+  root :to => 'static_pages#home'
 
   # See how all your routes lay out with "rake routes"
 
