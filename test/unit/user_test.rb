a5925b94536e181a88974e0b2a3b0cba40a5fe6f
Aldin Milanovic <aldin@atlantbh.com>
2015-06-21 14:17:50 +0200

    Small change

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -2,15 +2,15 @@
 #
 # Table name: users
 #
-#  id         :integer          not null, primary key
-#  firstName  :string(255)
-#  lastName   :string(255)
-#  address    :string(255)
-#  username   :string(255)
-#  password   :string(255)
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#  email      :string(255)
+#  id              :integer          not null, primary key
+#  firstName       :string(255)
+#  lastName        :string(255)
+#  address         :string(255)
+#  username        :string(255)
+#  password_digest :string(255)
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  email           :string(255)
 #
 
 require 'test_helper'
